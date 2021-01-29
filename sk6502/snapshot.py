4b00b127ec90694315ce5b67de60e9329e75b13e
Richard Dymond <rjdymond@gmail.com>
2021-01-29 17:25:08 -0400

    Add support to the VSF snapshot reader for VIC20MEM modules

--- sk6502/snapshot.py
+++ sk6502/snapshot.py
@@ -2,7 +2,7 @@ from skoolkit.snapshot import SnapshotError
 
 # https://vice-emu.sourceforge.io/vice_9.html
 VSF_MAGIC = b'VICE Snapshot File\x1a'
-C64MEM = b'C64MEM\x00'
+MEM_MODULES = (b'C64MEM\x00', b'VIC20MEM\x00')
 
 def can_read(fname):
     return fname[-4:].lower() == '.vsf'
@@ -16,8 +16,12 @@ def get_snapshot(fname, page=None):
 
     i = 58
     while i < len(data):
-        if data[i:i + len(C64MEM)] == C64MEM:
-            return list(data[i + 26:i + 65562])
-        i += data[i + 18] + 256 * data[i + 19] + 65536 * data[i + 20] + 16777216 * data[i + 21]
+        mod_size = data[i + 18] + 256 * data[i + 19] + 65536 * data[i + 20] + 16777216 * data[i + 21]
+        for name in MEM_MODULES:
+            if data[i:i + len(name)] == name:
+                mem = list(data[i + 26:i + mod_size])
+                mem.extend([0] * (65536 - len(mem)))
+                return mem
+        i += mod_size
 
-    raise SnapshotError('{}: C64MEM module not found'.format(fname))
+    raise SnapshotError('{}: No supported memory module found'.format(fname))
--- tests/test_snapshot.py
+++ tests/test_snapshot.py
@@ -20,17 +20,35 @@ class SnapshotReaderTest(unittest.TestCase):
     def test_can_read(self):
         self.assertTrue(can_read('snapshot.vsf'))
 
-    def test_get_snapshot(self):
+    def test_get_snapshot_c64(self):
         vsf = bytearray(b'VICE Snapshot File\x1a')
         vsf.extend([0] * (58 - len(vsf)))
         vsf.extend(b'C64MEM')
-        vsf.extend([0] * (84 - len(vsf)))
+        vsf.extend([0] * (76 - len(vsf)))
+        vsf.extend((26, 0, 1, 0)) # Module size
+        vsf.extend((0, 0, 0, 0))
         ram = [b & 255 for b in range(65536)]
         vsf.extend(ram)
         with open(TMP_FILE, 'wb') as f:
             f.write(vsf)
         snapshot = get_snapshot(TMP_FILE)
+        self.assertEqual(len(snapshot), 65536)
         self.assertEqual(ram, snapshot)
+
+    def test_get_snapshot_vic20(self):
+        vsf = bytearray(b'VICE Snapshot File\x1a')
+        vsf.extend([0] * (58 - len(vsf)))
+        vsf.extend(b'VIC20MEM')
+        vsf.extend([0] * (76 - len(vsf)))
+        vsf.extend((26, 20, 0, 0)) # Module size
+        vsf.extend((0, 0, 0, 0))
+        ram = [b & 255 for b in range(5120)]
+        vsf.extend(ram)
+        with open(TMP_FILE, 'wb') as f:
+            f.write(vsf)
+        snapshot = get_snapshot(TMP_FILE)
+        self.assertEqual(len(snapshot), 65536)
+        self.assertEqual(ram, snapshot[:len(ram)])
 
     def test_non_vsf(self):
         with open(TMP_FILE, 'wb') as f:
@@ -38,11 +56,11 @@ class SnapshotReaderTest(unittest.TestCase):
         with self.assertRaisesRegex(SnapshotError, '{}: Not a VICE snapshot file'.format(TMP_FILE)):
             get_snapshot(TMP_FILE)
 
-    def test_no_c64mem(self):
+    def test_no_memory_module(self):
         bad_vsf = bytearray(b'VICE Snapshot File\x1a')
         bad_vsf.extend([0] * (76 - len(bad_vsf)))
         bad_vsf.extend((22, 0, 0, 0))
         with open(TMP_FILE, 'wb') as f:
             f.write(bad_vsf)
-        with self.assertRaisesRegex(SnapshotError, '{}: C64MEM module not found'.format(TMP_FILE)):
+        with self.assertRaisesRegex(SnapshotError, '{}: No supported memory module found'.format(TMP_FILE)):
             get_snapshot(TMP_FILE)
